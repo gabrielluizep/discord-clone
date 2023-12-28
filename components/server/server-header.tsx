522a34c1da0c319192d56ab9b25692d62cff5427
gabrielluizep <gabrielluizep@hotmail.com>
2023-12-28 10:45:07 -0300

    added create channel feature

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -65,6 +65,7 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
 
         {isModerator && (
           <DropdownMenuItem
+            onClick={() => onOpen("createChannel", { server })}
             className="px-3 py-2 text-sm cursor-pointer"
           >
             Create Channel
