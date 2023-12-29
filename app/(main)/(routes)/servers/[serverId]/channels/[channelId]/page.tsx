13f273af5f636058fa8b82ea2051385c9dac1fa2
gabrielluizep <gabrielluizep@hotmail.com>
2023-12-28 21:02:55 -0300

    created chat input

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -4,6 +4,7 @@ import { currentProfile } from "@/lib/current-profile"
 import { db } from "@/lib/db"
 import { redirect } from "next/navigation"
 import { ChatHeader } from "@/components/chat/chat-header"
+import { ChatInput } from "@/components/chat/chat-input"
 
 interface ChannelIdPageProps {
   params: {
@@ -40,6 +41,20 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
   return (
     <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
       <ChatHeader name={channel.name} serverId={channel.serverId} type="channel" />
+
+      <div className="flex-1">
+        messages
+      </div>
+
+      <ChatInput
+        name={channel.name}
+        type="channel"
+        apiUrl="/api/socket/mesages"
+        query={{
+          channelId: channel.id,
+          serverId: channel.serverId
+        }}
+      />
     </div>
   );
 }
